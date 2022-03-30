6e9daf03dbfd048f516c914e09bf40a8b62d0415
David Abril <David.Abril@uptc.edu.co>
2022-03-30 13:57:12 -0500

    Primer Commit

---
+++ pension-master/includes/config.php
@@ -0,0 +1,7 @@
+<?php
+$dbuser="u5kqcja6qbfwixja";
+$dbpass="vzKTAoGQctvrmZ3BuEoG";
+$host="bhxcvvnq9j6lo7aynk87-mysql.services.clever-cloud.com";
+$db="bhxcvvnq9j6lo7aynk87";
+$mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+?>
